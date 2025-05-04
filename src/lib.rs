ee278ca1dd3209a146323da565af66dc4696f020
nioe <eoinmitchell39@proton.me>
2025-05-04 13:47:34 -0400

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use actix_web::{
+    error, get,
+    http::{header::ContentType, StatusCode},
+    App, HttpResponse, HttpServer,
+};
+use derive_more::derive::{Display, Error};
+
+
+
+#[derive(Debug, derive_more::From, Display, Error)]
+pub enum Error {
+
+    #[from]
+    #[display("Internal server error")]
+    Actix(actix_web::Error),
+
+    #[display("{message}")]
+    BadRequest {
+        message: String,
+    }
+
+
+}
+
+
+impl error::ResponseError for Error {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::html())
+            .body(self.to_string())
+    }
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
